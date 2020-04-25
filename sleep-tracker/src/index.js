a085737997267cc35930d16181c034193f245459
alanblee <alanbenlee12@gmail.com>
2020-04-25 13:13:42 -0700

    more cleanup and setting up redux and react-router in index.js

--- sleep-tracker/src/index.js
+++ sleep-tracker/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import './index.scss';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
