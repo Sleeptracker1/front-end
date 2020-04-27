9c5a48932a273a1ffbe0ff0ad5acfbed7a11f6cb
alanblee <alanbenlee12@gmail.com>
2020-04-27 07:43:57 -0700

    structuring dashboard files

--- sleep-tracker/src/App.jsx
+++ sleep-tracker/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import DashContainer from "./components/dashboard/dashContainer";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <ThemeProvider>
+        <Route path="/user-dashboard" component={DashContainer} />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ sleep-tracker/src/components/dashboard/dashSidebar/dashSidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DashSidebar = () => {
+  return (
+    <div>
+      dashSidebar
+    </div>
+  )
+}
+export default DashSidebar;
