f6c6299b8c5d026c216f8d9409bd1e9e1c57aeca
alanblee <alanbenlee12@gmail.com>
2020-04-26 19:05:04 -0700

    building out user-dashboard

---
+++ sleep-tracker/src/components/dashboard/dashboardContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DashboardSidebar from "./sidebar";
+
+const DashboardContainer = () => {
+  return (
+    <div className="dashboard-container">
+      <DashboardSidebar />
+    </div>
+  )
+}
+export default DashboardContainer;
---
+++ sleep-tracker/src/components/dashboard/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DashboardSidebar = () => {
+  return (
+    <section className="sidebar">
+      <nav>test</nav>
+    </section>
+  );
+};
+export default DashboardSidebar;
