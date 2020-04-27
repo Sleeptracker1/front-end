fb2e53e49a09a75acc3bab81c707db1c030b130a
alanblee <alanbenlee12@gmail.com>
2020-04-27 13:37:59 -0700

    figured the data structure for the backend

--- sleep-tracker/src/components/sleepLog/sleepLogForm/sleepLogForm.jsx
+++ sleep-tracker/src/components/sleepLog/sleepLogForm/sleepLogForm.jsx
@@ -2,42 +2,64 @@ import React, { useState } from "react";
 import moment from "moment";
 
 const SleepLogForm = () => {
-  const [logInputs, setLogInputs] = useState({
+  const [formInputs, setFormInputs] = useState({
     startTime: "",
     startDate: "",
     endTime: "",
     endDate: "",
-    difference: "",
+    rating: 0,
+  });
+  const [fullDate, setFullDate] = useState({
+    startTime: "",
+    endTime: "",
+    // diff: "",
     rating: 0,
   });
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setLogInputs({
-      ...logInputs,
+    setFormInputs({
+      ...formInputs,
       [name]: value,
     });
   };
   const convertDate = () => {
-    let start = moment(`${logInputs.startDate}T${logInputs.startTime}`);
-    let end = moment(`${logInputs.endDate}T${logInputs.endTime}`);
-    let diff = end.diff(start);
+    let startTime = moment(`${formInputs.startDate}T${formInputs.startTime}`);
+    let endTime = moment(`${formInputs.endDate}T${formInputs.endTime}`);
+    let diff = endTime.diff(startTime);
     let diffDuration = moment.duration(diff);
-    setLogInputs({
-      ...logInputs,
-      difference: diffDuration.hours(),
+
+    setFullDate({
+      ...fullDate,
+      startTime: startTime._i,
+      endTime: endTime._i,
+      // diff: diffDuration.hours(),
+      rating: formInputs.rating,
     });
   };
+
   return (
     <div>
       <label>start</label>
-      <input name="startDate" type="date" onChange={handleChange}></input>
-      <input name="startTime" type="time" onChange={handleChange}></input>
+      <input
+        name="startDate"
+        type="date"
+        onChange={handleChange}
+        min={moment(Date.now()).format("mm/dd/yyy")}
+      />
+      <input name="startTime" type="time" onChange={handleChange} />
       <br />
       <label>end</label>
-      <input name="endDate" type="date" onChange={handleChange}></input>
-      <input name="endTime" type="time" onChange={handleChange}></input>
-
+      <input
+        name="endDate"
+        type="date"
+        onChange={handleChange}
+        min={moment(Date.now()).format("mm/dd/yyy")}
+      />
+      <input name="endTime" type="time" onChange={handleChange} />
+      <input name="rating" value={formInputs.rating} onChange={handleChange} type="number" min="0" max="4" />
       <button onClick={convertDate}>convert time</button>
+      <p>time difference {fullDate.diff} hours</p>
+      {console.log(fullDate)}
     </div>
   );
 };
