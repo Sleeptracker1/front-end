48a5416d98d6d982d8c9780f0e07dcec0262e340
alanblee <alanbenlee12@gmail.com>
2020-04-27 12:44:37 -0700

    able to get the date difference with regular inputs and momentjs conversions

--- sleep-tracker/src/App.jsx
+++ sleep-tracker/src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import DashContainer from "./components/dashboard/dashContainer";
+import SleepLogForm from "./components/sleepLog/sleepLogForm/sleepLogForm";
 import { DashStyles } from "./styled-component/dashboardContainer";
 import { lightTheme, darkTheme } from "./styled-component/theme";
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={lightTheme}>
+      <SleepLogForm />
+
+      {/* <ThemeProvider theme={lightTheme}>
         <DashStyles />
         <Route path="/user-dashboard" component={DashContainer} />
-      </ThemeProvider>
+      </ThemeProvider> */}
     </div>
   );
 }
--- sleep-tracker/src/components/sleepLog/sleepLogForm/sleepLogForm.jsx
+++ sleep-tracker/src/components/sleepLog/sleepLogForm/sleepLogForm.jsx
@@ -1,33 +1,43 @@
 import React, { useState } from "react";
-import DateTimePicker from "react-datetime-picker";
+import moment from "moment";
 
 const SleepLogForm = () => {
-  const [startTime, setStartTime] = useState(new Date());
-  const [endTime, setEndTime] = useState(new Date());
   const [logInputs, setLogInputs] = useState({
     startTime: "",
+    startDate: "",
     endTime: "",
+    endDate: "",
+    difference: "",
     rating: 0,
   });
-  const startOnChange = (date) => {
-    setStartTime(date);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setLogInputs({
       ...logInputs,
-      startTime: startTime,
+      [name]: value,
     });
   };
-  const endOnChange = (date) => {
-    setEndTime(date);
+  const convertDate = () => {
+    let start = moment(`${logInputs.startDate}T${logInputs.startTime}`);
+    let end = moment(`${logInputs.endDate}T${logInputs.endTime}`);
+    let diff = end.diff(start);
+    let diffDuration = moment.duration(diff);
     setLogInputs({
       ...logInputs,
-      endTime: endTime,
+      difference: diffDuration.hours(),
     });
   };
   return (
     <div>
-      <DateTimePicker onChange={startOnChange} value={startTime} />
-      <DateTimePicker onChange={endOnChange} value={endTime} />
-      {console.log(Date.now())}
+      <label>start</label>
+      <input name="startDate" type="date" onChange={handleChange}></input>
+      <input name="startTime" type="time" onChange={handleChange}></input>
+      <br />
+      <label>end</label>
+      <input name="endDate" type="date" onChange={handleChange}></input>
+      <input name="endTime" type="time" onChange={handleChange}></input>
+
+      <button onClick={convertDate}>convert time</button>
     </div>
   );
 };
