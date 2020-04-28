8c6c8fff95d6cdd797defd7601f7ef65392a4fc1
miriam-sullivan <maljayd42@gmail.com>
2020-04-28 15:29:27 -0500

    messed with the graph

--- sleep-tracker/src/components/dashboard/dashContent/AddEditSleepForm.js
+++ sleep-tracker/src/components/dashboard/dashContent/AddEditSleepForm.js
@@ -11,17 +11,34 @@ export default function SleepList(props) {
     EndTime: "",
     Score: [],
   });
+  function difference(StartTime, EndTime) {
+    var start = moment(StartTime, "HH:mm");
+    console.log("difference", StartTime, EndTime);
+    var end = moment(EndTime, "HH:mm");
+    if (end.isBefore(start)) end.add(1, "day");
+
+    function diff(start, end) {
+      var diff = moment.duration(end.diff(start));
+      var res = moment(+diff).format("H:mm");
+      return localStorage.setItem(res);
+    }
+
+    return diff(start, end);
+  }
   const [loading, setLoading] = useState(false);
   // const apiURL = ""
   const AddDateTime = (e) => {
     // e.preventDefault();
-    const data = {
-      Date: dateTime.Date,
-      StartTime: dateTime.StartTime,
-      EndTime: dateTime.EndTime,
-      Score: dateTime.Score,
+    const data = () => {
+      setDateTime({
+        Date: dateTime.Date,
+        StartTime: dateTime.StartTime,
+        EndTime: dateTime.EndTime,
+        Score: dateTime.Score,
+      });
+      return difference(dateTime.StartTime, dateTime.EndTime), dateTime.Score;
     };
-    //   axiosWithAuth.post(apiURL, difference(data.StartTime, data.EndTime)).then((res) => {
+    //   axiosWithAuth.post(apiURL, data).then((res) => {
     //     props.history.push("/sleep-routine");
 
     // }
@@ -33,19 +50,6 @@ export default function SleepList(props) {
       [e.target.name]: e.target.value,
     });
   };
-  function difference(StartTime, EndTime) {
-    var start = moment(StartTime, "HH:mm");
-    console.log("difference", StartTime, EndTime);
-    var end = moment(EndTime, "HH:mm");
-    if (end.isBefore(start)) end.add(1, "day");
-    function diff(start, end) {
-      var diff = moment.duration(end.diff(start));
-      var res = moment(+diff).format("H:mm");
-      return localStorage.setItem(res);
-    }
-
-    return diff(start, end);
-  }
 
   return (
     <Grommet>
@@ -63,7 +67,6 @@ export default function SleepList(props) {
           <input
             type="date"
             name="Date"
-            selected={dateTime.Date}
             placeholder="Date"
             onChange={(e) => onChange(e)}
           />
@@ -79,6 +82,20 @@ export default function SleepList(props) {
             value={dateTime.EndTime}
             onChange={(e) => onChange(e)}
           />
+          <select>
+            <option id="1" value={dateTime.Score}>
+              😃
+            </option>
+            <option id="2" value={dateTime.Score}>
+              🙂
+            </option>
+            <option id="3" value={dateTime.Score}>
+              😐
+            </option>
+            <option id="4" value={dateTime.Score}>
+              😩
+            </option>
+          </select>
           <input
             type="submit"
             onClick={console.log(dateTime.StartTime, dateTime.EndTime)}
--- sleep-tracker/src/components/dashboard/dashContent/SleepGraph.js
+++ sleep-tracker/src/components/dashboard/dashContent/SleepGraph.js
@@ -1,5 +1,50 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { Distribution, Box, Grommet, Chart } from "grommet";
+import ClockLoader from "react-spinners/ClockLoader";
+import axiosWithAuth from "./utils/axiosWithAuth";
 export default function SleepGraph() {
-  return <h1>Graph</h1>;
+  const [data, setData] = useState({ sleepData: [], mode: "view" });
+  useEffect(() => {
+    // setIsLoading(true);
+    //axiosWithAuth()
+    //.get("")
+    //.then((res) => {
+    // console.log("res in sleep list,", res)
+    // setData({sleepData: res.data, mode: 'view'})
+    // setIsLoading(false)
+    // })
+  }, []);
+  return (
+    <Grommet>
+      {/* <Chart type="line" values={[
+    data.map((d => {
+      
+    }))
+  ]}> */}
+
+      {/* </Chart> */}
+      <Box
+        direction="column"
+        pad="small"
+        animation="slideLeft"
+        background="light-2"
+      >
+        <Chart
+          type="point"
+          bounds={[
+            [0, 4],
+            [0, 24],
+          ]}
+          values={[
+            { value: [7, 5], label: "4/20" },
+            { value: [6, 7], label: "4/21" },
+            { value: [5, 8], label: "4/22" },
+            { value: [4, 10], label: "4/23" },
+          ]}
+          aria-label="chart"
+        />
+        <Box direction="column"></Box>
+      </Box>
+    </Grommet>
+  );
 }
