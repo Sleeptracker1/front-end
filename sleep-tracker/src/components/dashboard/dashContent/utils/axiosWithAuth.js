55672c289a9ca8260370a3ce5de405a3f7935df1
alanblee <alanbenlee12@gmail.com>
2020-04-29 14:03:46 -0700

    removed all hard coded userId and tokens

--- sleep-tracker/src/components/dashboard/dashContent/utils/axiosWithAuth.js
+++ sleep-tracker/src/components/dashboard/dashContent/utils/axiosWithAuth.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
-  // returns an "instance" of axios, with preconfigured configs
-  // const token = JSON.parse(localStorage.getItem("token"));
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjo3LCJpYXQiOjE1ODgxNzE1OTgsImV4cCI6MTU4ODc3NjM5OH0.HLXXBP1YKkvT8L4RsVKo3owQ9qGrK3cvE-erBZnKOkQ";
+  const token = localStorage.getItem("token");
   return axios.create({
     headers: {
-      Authorization: token,
+      "Content-Type": "application/json",
+      Authorization: `${token}`,
     },
     baseURL: "https://bw-ft-sleep-tracker-1.herokuapp.com/",
   });
