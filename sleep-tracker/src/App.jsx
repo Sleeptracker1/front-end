a0b67895bc30675bd79ab0563d25a256c524419f
miriam-sullivan <maljayd42@gmail.com>
2020-04-29 19:05:01 -0500

    added a legend to the graph

--- sleep-tracker/src/App.jsx
+++ sleep-tracker/src/App.jsx
@@ -5,10 +5,8 @@ import { ThemeProvider } from "styled-components";
 import DashContainer from "./components/dashboard/dashContainer";
 import { DashStyles } from "./styled-component/dashboardContainer";
 import { lightTheme, darkTheme } from "./styled-component/theme";
-import { Grommet } from "grommet";
 import SleepDisplay from "./components/dashboard/dashContent/SleepDisplay";
 import AddEditSleepForm from "./components/dashboard/dashContent/AddEditSleepForm";
-import Links from "./components/dashboard/dashSidebar/Links";
 import PrivateRoute from "./components/PrivateRoute";
 import LoginForm from "./components/Forms/LoginForm";
 import RegistrationForm from "./components/Forms/RegistrationForm";
@@ -24,7 +22,7 @@ function App({ loggedIn }) {
           </li>
 
           <li>
-            <Link to={"/sleep-routine"}>Sleep Routine</Link>
+            <Route to={"/sleep-routine"}>Sleep Routine</Route>
           </li>
           <li>
             <PrivateRoute to={"/add-sleep-routine"}>
@@ -41,12 +39,8 @@ function App({ loggedIn }) {
             path="/user-dashboard"
             component={DashContainer}
           />
-          <PrivateRoute exact path="/sleep-routine" component={SleepDisplay} />
-          <PrivateRoute
-            exact
-            path="/add-sleep-routine"
-            component={AddEditSleepForm}
-          />
+          <Route exact path="/sleep-routine" component={SleepDisplay} />
+          <Route exact path="/add-sleep-routine" component={AddEditSleepForm} />
         </Switch>
       </ThemeProvider>
     </div>
