a4c23aca6e7320179eefe6e00fd26c32cefae6b9
alanblee <alanbenlee12@gmail.com>
2020-04-30 11:54:55 -0700

    removed some useless files. styling pf sidebar

--- sleep-tracker/src/components/dashboard/dashSidebar/dashSideMenu/menuItem.styled.js
+++ sleep-tracker/src/components/dashboard/dashSidebar/dashSideMenu/menuItem.styled.js
@@ -1,33 +1,32 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   border-left: 3px solid
     ${(props) => (props.active ? props.theme.activeMenu : "transparent")};
   width: 100%;
-  padding: 0.3rem;
-  padding-left: 2rem;
   cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin-bottom: 1rem;
   transition: 0.2s all ease-in-out;
+  margin: 15% 0;
+`;
 
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  text-align: center;
+  color: #536dfe;
+  transition: 0.2s color ease;
+  &:visited,
+  actiive {
+    color: #536dfe;
+  }
   &:hover {
-    background-color: rgba(0, 0, 0, 0.1);
+    color: #00b0ff;
   }
 `;
 
-// export const Span = styled.span`
-//     /* color: ${(props) =>
-//       props.active ? props.theme.activeMenu : "#AAA5A5"}; */
-//     color: ${(props) => !props.active && props.theme.textColor};
-//     font-size: 1rem;
-//     margin-right: 1rem;
-// `;
-
-// export const Title = styled.h1`
-//   font-size: 0.9rem;
-//   font-weight: 300;
-//   color: ${(props) => (props.active ? props.theme.activeMenu : "#AAA5A5")};
-// `;
+export const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
